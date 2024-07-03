67b631f2240ecfae17e4f4e0536899809febd570
Joseph Morales <josephzx1@hotmail.com>
2024-07-02 18:48:28 -0600

    feat; implement error

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -3,16 +3,14 @@ import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
-    constructor(
-        private readonly carsService: CarsService
-    ) { }
-    @Get()
-    findAll() {
-        return this.carsService.find()
-    }
-    @Get(':id')
-    findOne(@Param('id', ParseIntPipe) id: number) {
-        const car = this.carsService.findOne(id) || 'not found'
-        return car
-    }
+  constructor(private readonly carsService: CarsService) {}
+  @Get()
+  findAll() {
+    return this.carsService.find();
+  }
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    const car = this.carsService.findOne(id) || 'not found';
+    return car;
+  }
 }
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,26 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 export interface Car {
-    id: number;
-    brand: string;
-    model: string
+  id: number;
+  brand: string;
+  model: string;
 }
 @Injectable()
 export class CarsService {
+  private cars: Car[] = [
+    { id: 1, brand: 'Toyota', model: 'Camry' },
+    { id: 2, brand: 'Honda', model: 'Civic' },
+    { id: 3, brand: 'Ford', model: 'Mustang' },
+  ];
 
-    private cars: Car[] = [
-        { "id": 1, "brand": "Toyota", "model": "Camry" },
-        { "id": 2, "brand": "Honda", "model": "Civic" },
-        { "id": 3, "brand": "Ford", "model": "Mustang" }
-    ]
-
-    find() {
-        return this.cars
-    }
-    findOne(id: number) {
-        const car = this.cars.find((car) => {
-
-            return car.id === id
-        })
-        return car
-    }
+  find() {
+    return this.cars;
+  }
+  findOne(id: number) {
+    const car = this.cars.find((car) => {
+      return car.id === id;
+    });
+    if (car) throw new NotFoundException('Car not found');
+    return car;
+  }
 }
